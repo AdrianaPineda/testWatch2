851b03c8aaa6e6118de57da464d2fed97ce2ffa1
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-06-09 17:02:55 -0500

    Add Podfile and configure OpenSSL

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'testWatch2' do
+
+end
+
+target 'watch app' do
+
+end
+
+target 'watch app Extension' do
+    platform :watchos, '2.0'
+    xcodeproj 'testWatch2.xcodeproj'
+    pod 'OpenSSL', '1.0.206'
+end
+
--- testWatch2/ViewController.swift
+++ testWatch2/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        self.testAssignment()
-        self.testReference(["1"])
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,10 +23,9 @@ class ViewController: UIViewController {
     @IBAction func update(sender: AnyObject) {
 
         var tokens: [TokenDTO] = []
-
         
-        let token1 = TokenDTO(withName: "d", logo: "a", secretSeed: "a")
-        let token2 = TokenDTO(withName: "d", logo: "b", secretSeed: "b")
+        let token1 = TokenDTO(withName: "a", logo: "c", secretSeed: "a")
+        let token2 = TokenDTO(withName: "b", logo: "d", secretSeed: "a")
 
         tokens.append(token1)
         tokens.append(token2)
@@ -39,28 +35,6 @@ class ViewController: UIViewController {
         } else {
             // Fallback on earlier versions
         }
-    }
-
-    func testAssignment() {
-
-        let array1 = ["1", "2"]
-
-        var array2 = array1
-
-        array2.append("3")
-
-        print(array1)
-        print(array2)
-    }
-
-    func testReference(array: [String]) {
-
-        var array2 = array
-
-        array2.append("3")
-
-        print(array)
-        print(array2)
     }
 
 }
